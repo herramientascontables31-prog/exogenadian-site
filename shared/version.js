a364d9b97b24f303ab23c9d0d5b3603fc9bfe594
juanhoyos-maker <herramientascontables31@gmail.com>
2026-06-12 22:21:32 -0500

    Publicación 2026-06-12 22:21

--- shared/version.js
+++ shared/version.js
@@ -3,6 +3,6 @@ window.EXOGENA_VERSION = {
   commit: "16e30b1d8afc3c9152cee06b64cd20d2823b6b09",
   shortHash: "16e30b1d",
   commitDate: "2026-06-12T12:50:50-05:00",
-  buildDate: "2026-06-12T17:51:20.004Z",
+  buildDate: "2026-06-13T03:20:10.322Z",
   repoUrl: "https://github.com/herramientascontables31-prog/exogena-dian-2025"
 };
